480b46b108fc727bbeda270e0a45615dfa12f4a2
Matthewduke <mduke@usc.edu>
2019-02-28 00:51:47 -0800

    Adding JSX Components, Will be completed tomorrow.
    This is restructuring a previous API

---
+++ BusinessListA.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './BusinessListB.css';
+import Business_logic from '../Business/Business_logic';
+
+const Business extends React.Component{
+    render(){
+    <div className="BusinessList">
+  <Business />
+  <Business />
+  <Business />
+  <Business />
+  <Business />
+  <Business />
+</div>
+
+    }
+
+}
+
+export default BusinessListA;
---
+++ SearchBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './SearchBar_style.css';
+
+const sortByOptions = {
+
+    "Best Match":[
+
+
+
+
+    ],
+    "Highest Rated":[
+
+
+
+    ],
+    "Most Reviewed":[
+
+
+     
+    ]
+
+
+};
