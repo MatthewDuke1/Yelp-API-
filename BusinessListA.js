90df948747b135777fcea50ab1861b10927d3944
Matthewduke <mduke@usc.edu>
2019-03-01 01:32:33 -0800

    Test

--- BusinessListA.js
+++ BusinessListA.js
@@ -5,12 +5,13 @@ import Business_logic from '../Business/Business_logic';
 const Business extends React.Component{
     render(){
     <div className="BusinessList">
-  <Business />
-  <Business />
-  <Business />
-  <Business />
-  <Business />
-  <Business />
+  {
+
+    this.props.businesses.map((business) =>{
+      return <Business business={business} />;
+    });
+
+  }
 </div>
 
     }
